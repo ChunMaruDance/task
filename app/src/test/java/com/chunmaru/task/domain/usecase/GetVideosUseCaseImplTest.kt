dfb9ae37c53b7f290c0af3b3e1cc167c18bb9917
MVY <MVY@d.c>
2025-01-03 14:20:42 +0200

    fix tests

--- app/src/test/java/com/chunmaru/task/domain/usecase/GetVideosUseCaseImplTest.kt
+++ app/src/test/java/com/chunmaru/task/domain/usecase/GetVideosUseCaseImplTest.kt
@@ -1,10 +1,11 @@
 package com.chunmaru.task.domain.usecase
 
 import com.chunmaru.task.data.model.Video
-import com.chunmaru.task.data.repository.FakeVideoRepositoryImpl
 import com.chunmaru.task.domain.repository.VideoRepository
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
@@ -12,47 +13,48 @@ import org.junit.Test
 class GetVideosUseCaseImplTest {
 
     private lateinit var getVideosUseCase: GetVideosUseCase
-//    private lateinit var fakeVideoRepositoryImpl: FakeVideoRepositoryImpl
-
     private val videoRepository = mockk<VideoRepository>(relaxed = true)
+
+    private val testList = getTestList()
 
     @Before
     fun setUp() {
-
-//        fakeVideoRepositoryImpl = FakeVideoRepositoryImpl()
-
-        val testList = mutableListOf<Video>()
-
-        ('a'..'z').forEachIndexed { index, c ->
-            testList.add(
-                Video(
-                    description = "none",
-                    subtitle = "sub$index",
-                    thumb = c.toString(),
-                    sources = emptyList(),
-                    title = index.toString()
-                )
-            )
-        }
-
-//        runBlocking {
-//            fakeVideoRepositoryImpl.saveVideos(testList)
-//        }
-
-        coEvery { videoRepository.getVideos() } returns testList
-
         getVideosUseCase = GetVideosUseCaseImpl(videoRepository)
-
     }
-
 
     @Test
-    fun `stupid test GetVideosUseCaseImpl, except list of Videos`() = runBlocking {
+    fun `should fetch videos from API when no data in DB`() = runBlocking {
 
-        val data = getVideosUseCase.invoke()
-        assert(videoRepository.getVideos() == data)
+        coEvery { videoRepository.getVideosFromApi() } returns testList
 
+        val result = getVideosUseCase.invoke()
+
+        coVerify { videoRepository.getVideosFromApi() }
+        assertEquals(testList, result)
     }
 
+    @Test
+    fun `should fetch videos from DB when API call fails`() = runBlocking {
 
+        coEvery { videoRepository.getVideosFromApi() } throws Exception("API error")
+        coEvery { videoRepository.getVideosFromDb() } returns testList
+
+        val result = getVideosUseCase.invoke()
+
+        coVerify { videoRepository.getVideosFromApi() }
+        coVerify { videoRepository.getVideosFromDb() }
+        assertEquals(testList, result)
+    }
+
+    private fun getTestList(): List<Video> {
+        return ('a'..'z').mapIndexed { index, c ->
+            Video(
+                description = "none",
+                subtitle = "sub$index",
+                thumb = c.toString(),
+                sources = emptyList(),
+                title = index.toString()
+            )
+        }
+    }
 }
